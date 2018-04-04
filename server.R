4c386fde0b06c423c6edc5f414e7364599f54500
Haylee Hicks <hdhicks42@students.tntech.edu>
2018-04-03 21:43:53 -0500

    Fixing Server.R even more

--- server.R
+++ server.R
@@ -31,20 +31,5 @@ shinyServer(
     
   }
 )
-# server.R
-source("helpers.R")
-counties <- readRDS("data/counties.rds")
-library(maps)
-library(mapproj)
 
-shinyServer(
-  function(input, output) {
-    
-    output$map <- renderPlot({
-      
-      percent_map(counties, color = "blue")
-    })
-      
-  }
-)
     
